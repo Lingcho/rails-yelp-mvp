13d919b6752a8debbff5afa111aa4a5009b89be5
Van Say Nguyen <vansay@hotmail.fr>
2019-06-20 20:34:40 +0200

    Ok

---
+++ app/models/review.rb
@@ -0,0 +1,12 @@
+class Review < ApplicationRecord
+  RATINGS = [0, 1, 2, 3, 4, 5]
+  belongs_to :restaurant
+  validates :content, presence: true
+  validates :rating, inclusion: { in: RATINGS }, numericality: :only_integer
+end
+
+# validates :rating, presence: true,
+#                    numericality: {
+#                    greater_than_or_equal_to: 0,
+#                    less_than_or_equal_to: 5
+#                    }
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds.rb
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  'Buzzy destination for Indian street food in Bombay-style vintage decor.',
+    category:      'french'
+  },
+
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+    category:        'japanese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
